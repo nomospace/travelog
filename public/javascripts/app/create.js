d794c1ea9e5f7730a9d16d3421deb239ae06df70
nomospace <jinlu_hz@163.com>
2012-12-05 00:39:15 +0800

    Setup from-point and to-point with gmaps api

--- public/javascripts/app/create.js
+++ public/javascripts/app/create.js
@@ -1,13 +1,21 @@
 $(function() {
   'use strict';
+  var $fromPosition = $('#J_from_position'),
+    $toPosition = $('#J_to_position'),
+    $generateRoute = $('#J_generate_route');
+
+  var marker = {},
+    currentPosition;
+
   var map = new GMaps({
     div: '#J_map',
     lat: 31.225394428808663,
     lng: 121.47675279999999
   });
+
   GMaps.geolocate({
     success: function(position) {
-      map.setCenter(position.coords.latitude, position.coords.longitude);
+      currentPosition = position;
     },
     error: function(error) {
       alert('Geolocation failed: ' + error.message);
@@ -16,15 +24,52 @@ $(function() {
       alert('Your browser does not support geolocation');
     },
     always: function() {
-//      alert('Done!');
     }
   });
-//  map.drawRoute({
-//    origin: [39.984756, 116.314095],
-//    destination: [39.984756, 116.314095],
-//    travelMode: 'driving',
-//    strokeColor: '#131540',
-//    strokeOpacity: 0.6,
-//    strokeWeight: 6
-//  });
+
+  function setCenter(position) {
+    map.setCenter(position.coords.latitude, position.coords.longitude);
+  }
+
+  function addMarker(type) {
+    var coords = currentPosition.coords;
+    marker[type] && map.removeMarker(marker[type]);
+    marker[type] = map.addMarker({
+      lat: coords.latitude,
+      lng: coords.longitude,
+      draggable: true,
+      dragend: generateRoute
+    });
+    generateRoute();
+    // BAD SMELL
+    setCenter(currentPosition);
+  }
+
+  function drawRoute(from, to) {
+    if (from && to) {
+      map.cleanRoute();
+      map.drawRoute({
+        origin: [from.$a, from.ab],
+        destination: [to.$a, to.ab],
+        travelMode: 'walking',
+        strokeColor: '#000',
+        strokeOpacity: 0.6,
+        strokeWeight: 6
+      });
+    }
+  }
+
+  function generateRoute() {
+    var from = marker.from && marker.from.position,
+      to = marker.to && marker.to.position;
+    drawRoute(from, to);
+  }
+
+  $fromPosition.click(function() {
+    addMarker('from');
+  });
+  $toPosition.click(function() {
+    addMarker('to');
+  });
+  $generateRoute.click(generateRoute);
 });
